9fc52e7936e126ac4a78bccf49fcadac31fcb692
K <kelseydrhoda@gmail.com>
2020-02-02 15:05:19 -0700

    Panic proof, cleans up all listeners, does nothing to passed in channels

--- ds/dynamic_select.go
+++ ds/dynamic_select.go
@@ -1,13 +1,15 @@
-package dysl
+package ds
 
-var unit interface{}
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 // DynamicSelect is a concurrency control structure likenable to a dynamic generic select statement with sane defaults.
 // Also allows additional case statements to be loaded in during runtime.
 // Once running, can accept additional ChannelEntry structs to add to the internal cases.
 // The increased flexibility comes with the caveat that it can only deal in interfaces.
-// If the cost of reflection is too high in the handler functions, consider using structs then serialize in and out of JSON, passing byte-slices through the channels.
-// Future API may offer BDynamicSelect.
 // If the cost of serialization is also too high, consider implementing a well-typed version of the select in the Forever/StateMachine functions.
 // The tiered structure ensures that first Kill commands are read, then Kill commands and one-time events (in this case, channels closing) are attended to, and finally the all sources of messages are consumed.
 // This may seem overly cautious, but a stream reading a message every few milliseconds will noisly ignore numerous kill commands if all channels are in a flat select.
@@ -21,16 +23,24 @@ type DynamicSelect struct {
 	OnKillAction func()
 
 	// A channel used to load additional cases into the DynamicSelect during runtime.
-	Load chan ChannelEntry
+	load chan ChannelEntry
 
 	// A list of channels to manange and how to manage them
 	Channels []ChannelEntry
 	// Aggregator used to pass through only one message at a time.
-	internal chan dsWrapper
+	aggregator chan dsWrapper
+
+	// done is used to inform all Listeners kill was heard.
+	done chan interface{}
+
+	// Aggregator used to pass through priority messages.
+	priorityAggregator chan dsWrapper
 	// Aggregator used to pass through close notifications.
 	onClose chan dsWrapper
 	// alive is used to inform listeners if the main routine has exited.
 	alive bool
+	// listenerWG is used in clean up to make sure all children process have exited.
+	listenerWG sync.WaitGroup
 }
 
 // ChannelEntry is utilized to handle writes to and closure of the channel.
@@ -46,14 +56,17 @@ type ChannelEntry struct {
 // by the associated channel. Blocking determines whether it will be run
 // in a goroutine (Blocking = false) or synchronously (Blocking = true), the
 // later blocking reading messages from the queue.
+// If priority is set to true. will be read during the priority phase.
 type HandlerEntry struct {
 	Func     func(i interface{})
 	Blocking bool
+	Priority bool
 }
 
 // OnCloseEntry is a function that will be called the associated channel closes.
 // Blocking determines whether it will be run in a goroutine (Blocking = false) or
 // synchronously (Blocking = true), the latter blocking reading messages from the queue.
+// If not blocking, is read during the priority tier.
 type OnCloseEntry struct {
 	Func     func()
 	Blocking bool
@@ -67,18 +80,22 @@ type dsWrapper struct {
 
 // NewDynamicSelect uses an action to take on kill command, along with a list of channels to manage and returns a fully initialize DynamicSelect.
 func NewDynamicSelect(onKillAction func(), channels []ChannelEntry) *DynamicSelect {
-	i := make(chan dsWrapper)
+	a := make(chan dsWrapper)
+	p := make(chan dsWrapper)
+	d := make(chan interface{})
 	k := make(chan interface{})
 	l := make(chan ChannelEntry)
 	o := make(chan dsWrapper)
 	return &DynamicSelect{
-		Kill:         k,
-		OnKillAction: onKillAction,
-		Load:         l,
-		Channels:     channels,
-		internal:     i,
-		onClose:      o,
-		alive:        true,
+		Kill:               k,
+		OnKillAction:       onKillAction,
+		load:               l,
+		Channels:           channels,
+		aggregator:         a,
+		done:               d,
+		priorityAggregator: p,
+		onClose:            o,
+		alive:              true,
 	}
 }
 
@@ -88,6 +105,9 @@ func NewDynamicSelect(onKillAction func(), channels []ChannelEntry) *DynamicSele
 // If a message is heard on the DynamicSelect's Kill channel, the select is halted and
 // all contained channels are closed.
 func (d *DynamicSelect) Forever() {
+	// Set up defer for clean up:
+	defer d.shutDown()
+
 	// Start funneling messages into aggregator.
 	go d.startListeners()
 
@@ -101,27 +121,66 @@ func (d *DynamicSelect) Forever() {
 	}
 }
 
+// Load either blocks until the given ChannelEntry is loaded into a running DynamicSelect
+// or informs via error that the DynamicSelect has halted.
+func (d *DynamicSelect) Load(c ChannelEntry) error {
+	if d.alive {
+		d.load <- c
+		return nil
+	}
+	return fmt.Errorf("DynamicSelect has either halted or is uninitialized.")
+}
+
+// Once all listeners hit done, exit.
+func (d *DynamicSelect) shutDown() {
+	if r := recover(); r != nil {
+		log.Printf("Recovered from panic in main DynamicSelect: %v\n", r)
+		log.Println("Attempting normal shutdown.")
+	}
+
+	// just making sure.
+	d.alive = false
+	// alert waiting listeners.
+	close(d.done)
+
+	// Tell the outside world we're done.
+	d.OnKillAction()
+
+	go d.drainChannels()
+
+	// Wait for internal listeners to halt.
+	d.listenerWG.Wait()
+
+	// Make it painfully clear to the GC.
+	close(d.aggregator)
+	close(d.priorityAggregator)
+	close(d.onClose)
+	close(d.load)
+}
+
 // First, check if a kill command was heard during the previous process...
 func (d *DynamicSelect) stateMachine() bool {
 	select {
 	case <-d.Kill:
-		d.OnKillAction()
 		return false
 
 	default:
-		return d.singleMessageState()
+		return d.priorityMessageState()
 	}
 }
 
-// Then, check if any channel closed (a one-time event) in addition to the kill command.
-func (d *DynamicSelect) singleMessageState() bool {
+// Then, check if any channel closed (a one-time event) in addition to priority events and the kill command.
+func (d *DynamicSelect) priorityMessageState() bool {
 	select {
 	case dsw := <-d.onClose:
 		d.handleOnClose(dsw)
 		return true
 
+	case dsw := <-d.priorityAggregator:
+		d.handleInternal(dsw)
+		return true
+
 	case <-d.Kill:
-		d.OnKillAction()
 		return false
 
 	default:
@@ -133,11 +192,15 @@ func (d *DynamicSelect) singleMessageState() bool {
 func (d *DynamicSelect) allMessageState() bool {
 	select {
 
-	case dsw := <-d.internal:
+	case dsw := <-d.priorityAggregator:
 		d.handleInternal(dsw)
 		return true
 
-	case next := <-d.Load:
+	case dsw := <-d.aggregator:
+		d.handleInternal(dsw)
+		return true
+
+	case next := <-d.load:
 		// Grab the current len, and thus next index.
 		nextIndex := len(d.Channels)
 		// Add next
@@ -151,7 +214,6 @@ func (d *DynamicSelect) allMessageState() bool {
 		return true
 
 	case <-d.Kill:
-		d.OnKillAction()
 		return false
 	}
 }
@@ -167,47 +229,73 @@ func (d *DynamicSelect) startListeners() {
 // Start listener either passes messages to the aggregator channels or calls handlers locally
 // Depending on the entry supplied.
 func (d *DynamicSelect) startListener(i int, e ChannelEntry) {
+	d.listenerWG.Add(1)
+
+	// Clean up on close.
+	defer func() {
+		// We don't control the channels passed in. We may hit a runtime panic if they are closed.
+		if r := recover(); r != nil {
+			log.Printf("Recovered but exiting in DynamicSelect select listener. Likely attempted to read on a closed channel, error: %v\n", r)
+		}
+
+		// check for Blocking
+		if !e.OnClose.Blocking {
+			go e.OnClose.Func()
+		}
+
+		// Otherwise pass to main handler
+		var unit interface{}
+		lastMessage := dsWrapper{
+			Index:  i,
+			Target: unit,
+		}
+		d.onClose <- lastMessage
+
+		// Free up the waitgroup for shutdown.
+		d.listenerWG.Done()
+	}()
+
 	for {
 		// If using non-blocking handlers, we must check the select
-		// we are a proxy of is still alive.
+		// we are a proxy of is still alive after the last process.
 		if !d.alive {
 			return
 		}
 
+		select {
+		// While waiting, listen for overarching kill command.
+		case <-d.done:
+			return
 		// block to hear the channel.
-		x, ok := <-e.Channel
+		case x, ok := <-e.Channel:
 
-		// break when the channel is closed
-		if !ok {
-			// check for Blocking
-			if !e.OnClose.Blocking {
-				e.OnClose.Func()
+			// break when the channel is closed
+			if !ok {
+				// by returning here, we do not propegate
+				// the 0 value emmited on channel closure.
 				return
 			}
 
-			// Otherwise pass to main handler
-			lastMessage := dsWrapper{
-				Index:  i,
-				Target: unit,
+			// check for Blocking. If not handle locally.
+			if !e.Handler.Blocking {
+				go e.Handler.Func(x)
+				continue
 			}
-			d.onClose <- lastMessage
-			// by returning here, we do not propegate
-			// the 0 value emmited on channel closure.
-			return
-		}
 
-		// check for Blocking. If not handle locally.
-		if !e.Handler.Blocking {
-			go e.Handler.Func(x)
-			continue
-		}
+			// otherwise, pass through the value to the main listener.
+			message := dsWrapper{
+				Index:  i,
+				Target: x,
+			}
 
-		// otherwise, pass through the value to the main listener.
-		message := dsWrapper{
-			Index:  i,
-			Target: x,
+			// based on priority
+			if e.Handler.Priority {
+				d.priorityAggregator <- message
+				continue
+			}
+
+			d.aggregator <- message
 		}
-		d.internal <- message
 	}
 }
 
@@ -223,4 +311,50 @@ func (d *DynamicSelect) handleOnClose(dsw dsWrapper) {
 	entry := d.Channels[dsw.Index]
 
 	entry.OnClose.Func()
+}
+
+// Looks awful, but drains all channels in the DynamicSelect while waiting for the WG
+// to synchronize with the listeners, then close the channels.
+func (d *DynamicSelect) drainChannels() {
+	go func() {
+		for {
+			_, ok := <-d.aggregator
+			if ok {
+				continue
+			}
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			_, ok := <-d.priorityAggregator
+			if ok {
+				continue
+			}
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			_, ok := <-d.onClose
+			if ok {
+				continue
+			}
+			return
+		}
+	}()
+
+	// At this point, any call to d.Load will return an error, so we can safely
+	// Discard these as outstanding requests that will never be filled.
+	go func() {
+		for {
+			_, ok := <-d.load
+			if ok {
+				continue
+			}
+			return
+		}
+	}()
 }
--- example/ds/dynamic_select_example.go
+++ example/ds/dynamic_select_example.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/krhoda/goconquer/dysl"
+	"github.com/krhoda/goconquer/ds"
 	"github.com/krhoda/goconquer/example/helpers/bots"
 )
 
@@ -17,70 +17,74 @@ func main() {
 	ch1 := make(chan interface{})
 	ch2 := make(chan interface{})
 
-	chSl := []dysl.ChannelEntry{
+	chSl := []ds.ChannelEntry{
 		{
 			Channel: ch1,
-			Handler: dysl.HandlerEntry{
+			Handler: ds.HandlerEntry{
 				Func:     bots.HandleStringBot,
-				Blocking: true,
+				Blocking: false,
 			},
-			OnClose: dysl.OnCloseEntry{
+			OnClose: ds.OnCloseEntry{
 				Func:     func() {},
-				Blocking: true,
+				Blocking: false,
 			},
 		},
 		{
 			Channel: ch2,
-			Handler: dysl.HandlerEntry{
+			Handler: ds.HandlerEntry{
 				Func:     bots.HandleMathBot,
-				Blocking: true,
+				Blocking: false,
 			},
-			OnClose: dysl.OnCloseEntry{
+			OnClose: ds.OnCloseEntry{
 				Func:     func() {},
-				Blocking: true,
+				Blocking: false,
 			},
 		},
 	}
 
-	ds := dysl.NewDynamicSelect(ka, chSl)
+	sMgr := ds.NewDynamicSelect(ka, chSl)
 
 	go func() {
 		k := make(chan os.Signal, 1)
 		<-k
-		close(ds.Kill)
+		close(sMgr.Kill)
 	}()
 
-	go bots.MakeStringBot(ch1, ds.Kill)
-	go bots.MakeMathBot(ch2, ds.Kill)
+	go bots.MakeStringBot(ch1, sMgr.Kill)
+	go bots.MakeMathBot(ch2, sMgr.Kill)
 
-	go ds.Forever()
+	go sMgr.Forever()
 
 	time.Sleep(time.Second * 5)
 	log.Println("Main thread building Rune Bot...")
 	ch3 := make(chan interface{})
-	ce3 := dysl.ChannelEntry{
+	ce3 := ds.ChannelEntry{
 		Channel: ch3,
-		Handler: dysl.HandlerEntry{
+		Handler: ds.HandlerEntry{
 			Func:     bots.HandleRuneBot,
-			Blocking: true,
+			Blocking: false,
 		},
-		OnClose: dysl.OnCloseEntry{
+		OnClose: ds.OnCloseEntry{
 			Func:     func() {},
-			Blocking: true,
+			Blocking: false,
 		},
 	}
 
 	go func() {
-		ds.Load <- ce3
+		err := sMgr.Load(ce3)
+		if err != nil {
+			log.Println("Error in Load: %s\n", err)
+		}
 	}()
 
-	go bots.MakeRuneBot(ch3, ds.Kill)
+	go bots.MakeRuneBot(ch3, sMgr.Kill)
 
 	log.Println("Main thread has dispatch load message and rune bot...")
-
 	time.Sleep(time.Second * 30)
+
 	log.Println("...Main thread turning off other sevices...")
-	close(ds.Kill)
+	close(sMgr.Kill)
+
 	time.Sleep(time.Second * 5)
 	log.Println("...Main thread exiting, other services off.")
 }
